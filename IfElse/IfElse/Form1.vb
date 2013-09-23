5afef96d43d43c819f9f5b152edef99ee3304006
idsc3101 <idsc3101@gmail.com>
2013-09-23 17:24:01 -0500

    I worked on Day 3

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -4,16 +4,44 @@ Public Class Form1
 
     Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
         Dim num1, num2, largerNum As Double
+
+        'if user did wrong thing, then msgBox and eliminate them
+
+        If Not (IsNumeric(txtFirstNum.Text) And IsNumeric(txtSecondNum.Text)) Then 'imporrant that the inumber sections are in ()
+            MessageBox.Show("Error: Please provide a number")
+            Return
+        End If
+
         num1 = CDbl(txtFirstNum.Text)
         num2 = CDbl(txtSecondNum.Text)
 
-        If (num1 > num2) Then
-            largerNum = num1
-        Else
-            largerNum = num2
+
+        'If user did the wrong thing, then msgbox and eliminate them
+
+        If (num1 < 0 Or num2 < 0) Then
+            MessageBox.Show("error: please type a positive number")
+            Return
         End If
 
-        txtResult.Text = "the larger number is..." & largerNum
+
+
+        If (num1 > num2) Then
+            largerNum = num1
+
+            txtResult.Text = "the larger number is..." & num1
+
+
+
+        ElseIf (num1 < num2) Then
+            largerNum = num2
+
+            txtResult.Text = "the larger number is..." & num2
+
+
+        End If
+
+
+
 
     End Sub
 End Class
